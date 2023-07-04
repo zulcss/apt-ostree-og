7fee4e7081b207301c0028081b4d3aac2cab815e
Charles Short <zulcss@gmail.com>
2023-07-04 06:12:12 -0400

    split up config file
    Signed-off-by: Charles Short <zulcss@gmail.com>

--- apt_ostree/config.py
+++ apt_ostree/config.py
@@ -1,3 +1,5 @@
+import sys
+
 from rich.console import Console
 import yaml
 
@@ -11,10 +13,14 @@ class Config(object):
             with open(config, "r") as f:
                 cfg = yaml.safe_load(f)
         except IOError:
-            self.console.print("Unable to parse configuration file, using defaults.")
+            pass
+
+        rootfs = cfg.get("rootfs", None)
+        if rootfs is None:
+            self.console.print("[red]Unable to parse rootfs config, using defaults.[/red]")
 
         return {
-            "suite": cfg.get("suite", "bookwork"),
-            "branch": cfg.get("branch", "debian/bookworm"),
-            "repo": cfg.get("repo", "ostree_repo"),
+            "suite": rootfs.get("suite", "bookwork"),
+            "branch": rootfs.get("branch", "debian/bookworm"),
+            "repo": rootfs.get("repo", "ostree_repo"),
         }
