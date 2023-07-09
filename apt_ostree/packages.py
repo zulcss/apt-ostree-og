199066d2608e51456c88307a5501bc0e38c86f7b
Charles Short <zulcss@gmail.com>
2023-07-09 21:47:37 +0000

    show dependences and packages
    in a table
    
    Signed-off-by: Charles Short <zulcss@gmail.com>

--- apt_ostree/packages.py
+++ apt_ostree/packages.py
@@ -2,7 +2,9 @@ import sys
 import subprocess
 import apt
 
+import click
 from rich.console import Console
+from rich.table import Table
 
 from apt_ostree.constants import WORKSPACE
 from apt_ostree.ostree import Ostree
@@ -40,36 +42,106 @@ class Packages(object):
     def install(self, packages):
         """Install package"""
         self._apt_cache = self._cache()
-        deps = [] + list(packages)
+        deps = set()
+        predeps = set()
+        all_deps = set()
 
         self.packages = self.get_packages(packages)
         if len(self.packages) == 0:
             self.console.print("No valid packages found...exiting")
             sys.exit(-1)
 
-        self.ostree = Ostree(self.deployment_dir)
-        self.deployment_dir = self.ostree.deployment()
+        self.show_packages()
+        self.show_dependencies(all_deps, deps, predeps)
 
-        self.apt_update()
-        self.console.print("Installing the following package candidates")
+        if click.confirm("Do you want to continue?"):
+            self.ostree = Ostree(self.deployment_dir)
+            self.deployment_dir = self.ostree.deployment()
+
+            self.apt_update()
+            self.console.print("Installing the following package candidates")
+            for package in self.packages:
+                ver = self.get_version(package)
+                self.console.print(f"Installing {package} ({ver})")
+                self.apt_install(package)
+                self.post_install(package)
+
+            self.ostree.post_deployment()
+                self.post_install(package)
+
+
+    def show_dependencies(self, all_deps, deps, predeps): 
+        for pkg in self.packages:
+            deps = self.get_dependencies(
+                self._apt_cache,
+                self._apt_package(pkg),
+                deps,
+               "Depends")
+            predeps = self.get_dependencies(
+                self._apt_cache,
+                self._apt_package(pkg),
+                deps,
+               "PreDepends")
+        all_deps.update(deps, predeps)
+        all_deps = sorted(all_deps)
+
+        table = Table(title="New Dependencies", expand=True, box=None)
+        table.add_column("Name")
+        table.add_column("Version")
+        table.add_column("Architecture")
+        table.add_column("Component")
+        table.add_column("Origin")
+        table.add_column("Size")
+        for pkg in all_deps:
+            d = self._apt_package(pkg)
+            if not d.installed:
+                table.add_row(d.name,
+                              d.candidate.version,
+                              str(d.candidate.architecture),
+                              str(d.candidate.origins[0].archive),
+                              str(d.candidate.origins[0].origin),
+                              str(d.candidate.size))
+
+        self.console.print(table)
+
+         
+    def get_dependencies(self, cache, pkg, deps, key="Depends"):
+        candver = cache._depcache.get_candidate_ver(pkg._pkg)
+        if candver is None:
+            return deps
+        dependslist = candver.depends_list
+        if key in dependslist:
+            for depVerList in dependslist[key]:
+                for dep in depVerList:
+                    if dep.target_pkg.name in cache:
+                        if (
+                            pkg.name != dep.target_pkg.name
+                            and dep.target_pkg.name not in deps
+                        ):
+                            deps.add(dep.target_pkg.name)
+                            self.get_dependencies(cache, cache[dep.target_pkg.name], deps, key)
+        return deps
+
+    def show_packages(self):
+        table = Table(title="New Packages",expand=True, box=None)
+        table.add_column("Name")
+        table.add_column("Version")
+        table.add_column("Architecture")
+        table.add_column("Component")
+        table.add_column("Origin")
+        table.add_column("Size")
         for package in self.packages:
-            ver = self.get_version(package)
-            self.console.print(f"Installing {package} ({ver})")
-            self.apt_install(package)
-            self.post_install(package)
+            table.add_row(package, 
+                          self._apt_package(package).candidate.version,
+                          str(self._apt_package(package).candidate.architecture),
+                          str(self._apt_package(package).candidate.origins[0].archive),
+                          str(self._apt_package(package).candidate.origins[0].origin),
+                          str(self._apt_package(package).candidate.size))
+        self.console.print(table)
 
-        self.ostree.post_deployment()
 
     def post_install(self, package):
-        cache = apt.Cache(rootdir=self.deployment_dir)
-        pkg = cache[package].installed
-
-        pkgs = [
-            d[0].name
-            for d in pkg.get_dependencies("Depends", "PreDepends", "Recommends")
-        ]
-        self.console.print(pkgs)
-
+        pass
 
     def get_version(self, package):
         return self._apt_cache[package].candidate.version
