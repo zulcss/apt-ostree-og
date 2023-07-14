ba0e06331b35276266dff975b428a542f248c971
Charles Short <zulcss@gmail.com>
2023-07-14 14:31:27 +0000

    add front end for package updates
    Signed-off-by: Charles Short <zulcss@gmail.com>

--- apt_ostree/packages/upgrade.py
+++ apt_ostree/packages/upgrade.py
@@ -1,3 +1,8 @@
+import apt_pkg
+import sys
+
+from rich.table import Table
+
 class Upgrade:
     def upgrade(self):
         """"apt-get upgrade"""
@@ -7,3 +12,35 @@ class Upgrade:
         self.apt_upgrade()
 
         self.ostree.post_deployment()
+
+    def show_upgrades(self):
+        apt_pkg.init()
+        cache = apt_pkg.Cache()
+
+        depcache.init()
+        if depcache.broken_count > 0:
+            self.console.print("[red]Error [/red] Broken packages exists")
+            sys.exit(1)
+
+        # simulate an upgrade but dont actually do the upgrade
+        try:
+            depcache.upgrade(True)
+            if depcache.del_count > 0:
+                depcache.init()
+            debpcache.upgrade()
+        except SystemError as ex:
+            self.console.print(f"[red]Error: [/red] Couldn't mark the upgrade {e}")
+            sys.exit(0)
+
+        table = Table(title="Packages that need updating", expand=True, box=None)
+        table.add_column("Name")
+        table.add_column("Current Version")
+        table.add_column("New Version")
+        for pkg in cache.packages:
+            candidate = depcache.get_candidate_ver(pkg)
+            if depcache.marked_upgrade(pkg):
+                table.add_row(
+                    pkg.name,
+                    pkg.current_ver.ver_str,
+                    candidate.ver_str)
+        self.console.print(table)
